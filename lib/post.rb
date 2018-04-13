1ec2f63367a2f68da11435fcfa9000d51d935b68
Sara Tibbetts <sxt4866@rit.edu>
2018-04-13 00:05:35 +0000

    automatically backed up by learn

---
+++ lib/post.rb
@@ -0,0 +1,19 @@
+class Post
+
+  attr_accessor :title, :author 
+  
+  def initialize
+  end 
+  
+  # def author=(author)
+  #   @author = author 
+  # end 
+  
+  # def author 
+  #   @author 
+  # end 
+
+
+end 
+
+#post.author #=> <Author> 
