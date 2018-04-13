cfca7589d9cab09e36a16c883fa61af0ad1d7c4f
Sara Tibbetts <sxt4866@rit.edu>
2018-04-13 00:08:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,10 +4,21 @@ class Song
   # def artist
   #   @artist
   # end
-  # 
+  #
   # def artist=(artist)
   #   @artist = artist
   # end
-  # 
+  #
 
 end
+
+
+hello = Song.new 
+hello.title = "Hello"
+
+adele = Artist.new 
+artist.name = "Adele"
+
+hello.artist = "Adele"
+
+hello.artist #=> <Artist name="adele">
